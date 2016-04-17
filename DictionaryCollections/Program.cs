031b0214d72c78f440df72f9a068f6064d7a0528
csaszarhunor <csaszarhunor@gmail.com>
2016-04-17 16:30:48 +0200

    init

---
+++ DictionaryCollections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable lookup = new Hashtable();
+
+            lookup.Add("0", "Zero");
+            lookup.Add("1", "One");
+            lookup.Add("2", "Two");
+            lookup.Add("3", "Three");
+            lookup.Add("4", "Four");
+            lookup.Add("5", "Five");
+            lookup.Add("6", "Six");
+            lookup.Add("7", "Seven");
+            lookup.Add("8", "Eight");
+            lookup.Add("9", "Nine");
+
+            string number = "023453645";
+            foreach (char cter in number)
+            {
+                string digit = cter.ToString();
+                if (lookup.ContainsKey(digit))
+                {
+                    Console.WriteLine(lookup[digit]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SequentialCollections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            while (queue.Count > 0)
+            {
+                object ordNum = queue.Dequeue();
+                Console.WriteLine("From Queue: {0}", ordNum);
+            }
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object ordNum = stack.Pop();
+                Console.WriteLine("From Stack: {0}", ordNum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
